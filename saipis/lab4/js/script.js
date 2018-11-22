038842f3263bfc5078298d2afcd2d3270f46a6f0
Kiryl Klimovich <kirylklimovich@gmail.com>
2018-11-23 00:05:07 +0300

    lab4

---
+++ saipis/lab4/js/script.js
@@ -0,0 +1,59 @@
+document.getElementById('MainForm').onsubmit = function (event) {
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+    var names = getData('name', 'input');
+
+    tr.appendChild(generateData(names));
+    tr.appendChild(generateData(getData('date', 'input')));
+    tr.appendChild(generateData(getData('os', 'input')));
+    tr.appendChild(generateData(getData('brand', 'select')));
+    tr.appendChild(generateData(getData('isGreat', 'input')));
+    tr.appendChild(generateData(getData('otzyv', 'textarea')));
+    tr.appendChild(generateData(getData('tel', 'input')));
+    tr.appendChild(generateData(getData('email', 'input')));
+
+    document.getElementById('mainTable').appendChild(tr);
+
+    var namesOuterList = document.getElementById('namesOuterList');
+    var namesList = document.getElementById('namesList');
+    if (namesList == null) {
+        namesList = document.createElement('ol');
+        namesList.setAttribute('id', 'namesList');
+
+        var namesOuterMember = document.createElement('li');
+        namesOuterMember.appendChild(namesList);
+        namesOuterList.appendChild(namesOuterMember);
+    }
+
+    var li = document.createElement('li');
+    li.classList.add('orderedName');
+    li.innerText = names[0].value;
+    namesList.appendChild(li);
+
+    
+    function getData(inputName, elType) {
+        return document.querySelectorAll(elType + '[name=\"' + inputName + '\"]');
+    }
+
+    function generateData(data) {
+        var td = document.createElement('td');
+        console.log(data);
+        if (data.length === 1) {
+            td.innerText = data[0].value;
+        } else if (data.length > 1) {
+            var ul = document.createElement('ul');
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].type === 'radio' && !data[i].checked)
+                    continue;
+                if (data[i].type === 'checkbox' && !data[i].checked)
+                    continue;
+                var li = document.createElement('li');
+                li.innerText = data[i].value;
+                ul.appendChild(li);
+            }
+            td.appendChild(ul);
+        }
+        return td;
+    }
+}
